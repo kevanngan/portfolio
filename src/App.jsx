ecb5b26abbc8d185c770e7dd841bb97f598abc67
Kevan Ngan <kevanngan_@hotmail.com>
2024-04-29 22:23:28 -0700

    Add react router navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const restBase = 'https://kevanngan.com/portfolio/wp-json/wp/v2/';
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <Router basename= "/portfolio/">
+    <header className="site-header">
+      <nav className="site-navigation">
+        <ul>
+          <li><NavLink to='/' end>Home</NavLink></li>
+          <li><NavLink to='/projects'>Projects</NavLink></li>
+          <li><NavLink to='/about'>About</NavLink></li>
+          <li><NavLink to='contact'>Contact</NavLink></li>
+        </ul>
+      </nav>
+    </header>
+    <main class="main">
+      <Routes>
+        <Route path="/" element={<Home restBase={restBase}/>} />
+        <Route path="/projects" element={<Projects restBase={restBase}/>} />
+        <Route path="/about" element={<About restBase={restBase}/>} />
+        <Route path="/contact" element={<Contact restBase={restBase}/>} />
+      </Routes>
+    </main>
+   </Router>
   )
 }
 
