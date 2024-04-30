0089833ddb57ace48664ad429a5c95bfb2c93d17
Kevan Ngan <kevanngan_@hotmail.com>
2024-04-29 22:56:00 -0700

    Initial home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,20 @@
-const Home = () => {
+import React from 'react';
+import Header from './Header';
+import Hero from './Hero';
+import Projects from './Projects';
+import About from './About';
+import Footer from './Footer';
 
-}
+const Home = ({ restBase }) => {
+  return (
+    <div className="home-page">
+      <Header />
+      <Hero />
+      <Projects restBase={restBase} />
+      <About restBase={restBase} />
+      <Footer />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
