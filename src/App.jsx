c04eb903a42208d3e03ca329fc9bd3f06f778b4e
Kevan Ngan <kevanngan_@hotmail.com>
2024-04-29 23:27:41 -0700

    Fetch initial content for home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,37 @@
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import { NavLink } from 'react-router-dom'
-import Home from './components/Home'
-import About from './components/About'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-import Hero from './components/Hero'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Link as ScrollLink } from 'react-scroll';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
 
 function App() {
+  
   const restBase = 'https://kevanngan.com/portfolio/wp-json/wp/v2/';
 
   return (
-   <Router basename= "/portfolio">
-    <header className="site-header">
-      <nav className="site-navigation">
-        <ul>
-          <li><NavLink to='/' end>Home</NavLink></li>
-          <li><NavLink to='/projects'>Projects</NavLink></li>
-          <li><NavLink to='/about'>About</NavLink></li>
-          <li><NavLink to='contact'>Contact</NavLink></li>
-        </ul>
-      </nav>
-    </header>
-    <main class="main">
-      <Routes>
-        <Route path="/" element={<Home restBase={restBase}/>} />
-        <Route path="/projects" element={<Projects restBase={restBase}/>} />
-        <Route path="/about" element={<About restBase={restBase}/>} />
-        <Route path="/contact" element={<Contact restBase={restBase}/>} />
-      </Routes>
-    </main>
-   </Router>
-  )
+    <div>
+      <header className="site-header">
+        <nav className="site-navigation">
+          <ul>
+            {/* <li><NavLink to="/" exact>Home</NavLink></li> */}
+            <li><ScrollLink to="projects" smooth={true} duration={500}>Projects</ScrollLink></li>
+            <li><ScrollLink to="about" smooth={true} duration={500}>About</ScrollLink></li>
+          </ul>
+        </nav>
+      </header>
+      <main className="main">
+        <Router>
+          <Routes>
+            <Route path="/portfolio/" element={<Home restBase={restBase} />} />
+            <Route path="/about" element={<About restBase={restBase} />} />
+            <Route path="/projects" element={<Projects restBase={restBase} />} />
+          </Routes>
+        </Router>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header';
 import Hero from './Hero';
 import Projects from './Projects';
@@ -6,6 +6,27 @@ import About from './About';
 import Footer from './Footer';
 
 const Home = ({ restBase }) => {
+  const pageId = 7;
+  const restPath = `${restBase}pages/${pageId}`;
+
+  useEffect(() => {
+    const fetchPageData = async () => {
+      try {
+        const response = await fetch(restPath);
+        if (response.ok) {
+          const data = await response.json();
+          setPageData(data);
+        } else {
+          throw new Error('Failed to fetch data');
+        }
+      } catch (error) {
+        console.error('Error fetching page data:', error);
+      }
+    };
+
+    fetchPageData();
+  }, [restPath]);
+
   return (
     <div className="home-page">
       <Header />
