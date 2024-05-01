44829902fc8884256895fa440a82252b93222a25
Kevan Ngan <kevanngan_@hotmail.com>
2024-04-30 23:10:12 -0700

    Update projects content

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -22,6 +22,41 @@ const Project = ({ restBase }) => {
       {projectData && (
         <article id={`projects-section`}>
           <h1>My Projects</h1>
+          <div className="project-card-1">
+            <div className="project-image-container">
+              <a href="https://bigboyburgers.bcitwebdeveloper.ca/" target="_blank">
+                <img src="" alt="Big Boy Burgers Image" />  
+              </a> 
+            </div>
+            <div className="project-info">
+              <h3>Big Boy Burgers</h3>
+              <p className="project-description">Explore this collaborative capstone project, an online burger store built with WooCommerce on WordPress.</p>
+              <a href="" target="_blank">
+                <p>More Details</p>
+              </a>
+              <a href="https://github.com/htpwebdesign/bigboyburgers-theme" target="_blank">
+                <p>Go to GitHub</p>
+              </a>
+            </div>
+          </div>
+
+          <div className="project-card-2">
+            <div className="project-image-container">
+              <a href="https://kevanngan.com/showboat" target="_blank">
+                <img src="" alt="Showboat Image" />  
+              </a> 
+            </div>
+            <div className="project-info">
+              <h3>Showboat</h3>
+              <p className="project-description">A movie database project built with React.</p>
+              <a href="" target="_blank">
+                <p>More Details</p>
+              </a>
+              <a href="https://github.com/hojinchang/showboat" target="_blank">
+                <p>Go to GitHub</p>
+              </a>
+            </div>
+          </div>
         </article>
       )}
     </>
