522ae7f4ed74c85ebaf8eaa1e4a1142c47bb66ae
Kevan Ngan <kevanngan_@hotmail.com>
2024-05-01 01:12:21 -0700

    Split icons to 3 groups

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,3 +1,4 @@
+// TechStack.jsx
 import React from 'react';
 
 const techStackImages = [
@@ -7,20 +8,46 @@ const techStackImages = [
   'assets/icons/git.png',
   'assets/icons/nodejs.png',
   'assets/icons/php.png',
+  'assets/icons/sass.png',
   'assets/icons/react.png',
   'assets/icons/typescript.png',
   'assets/icons/wordpress.png',
 ];
 
-const TechStack = ({ images }) => {
+const TechStack = () => {
+  // Slice the images array into three groups
+  const firstGroup = techStackImages.slice(0, 3);
+  const secondGroup = techStackImages.slice(3, 7);
+  const thirdGroup = techStackImages.slice(7);
+
   return (
-    <div className="tech-stack">
-      {images.map((image, index) => (
-        <img key={index} src={image} alt={`Tech Stack ${index}`} />
-      ))}
+    <div className="tech-stack-wrapper">
+
+      <div className="stack-column-1">
+        {firstGroup.map((image, index) => (
+          <div key={index} className="tech-stack-item">
+            <img src={image} alt={`Tech Stack ${index}`} />
+          </div>
+        ))}
+      </div>
+
+      <div className="stack-column-2">
+        {secondGroup.map((image, index) => (
+          <div key={index} className="tech-stack-item">
+            <img src={image} alt={`Tech Stack ${index + 3}`} />
+          </div>
+        ))}
+      </div>
+
+      <div className="stack-column-3">
+        {thirdGroup.map((image, index) => (
+          <div key={index} className="tech-stack-item">
+            <img src={image} alt={`Tech Stack ${index + 7}`} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
 export { techStackImages, TechStack };
-
