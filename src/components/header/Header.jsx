195d1fa78bc41d898c524a61bec7035b65df820a
Kevan Ngan <kevanngan_@hotmail.com>
2024-05-02 23:40:36 -0700

    Import header content

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,17 @@
-const Header = () => {
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Link as ScrollLink } from 'react-scroll';
 
+function Header() {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/portfolio/">Home</Link></li>
+        <li><ScrollLink to="about-section" smooth={true} duration={500}>About</ScrollLink></li>
+        <li><ScrollLink to="projects-section" smooth={true} duration={500}>Projects</ScrollLink></li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Header
+export default Header;
