10b16ef0e20991e543e217fe82f9db424e637659
Kevan Ngan <kevanngan_@hotmail.com>
2024-05-02 23:51:07 -0700

    Fix header structure

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,14 +3,17 @@ import { Link } from 'react-router-dom';
 import { Link as ScrollLink } from 'react-scroll';
 
 function Header() {
+
   return (
-    <nav>
-      <ul>
-        <li><Link to="/portfolio/">Home</Link></li>
-        <li><ScrollLink to="about-section" smooth={true} duration={500}>About</ScrollLink></li>
-        <li><ScrollLink to="projects-section" smooth={true} duration={500}>Projects</ScrollLink></li>
-      </ul>
-    </nav>
+    <header>
+        <nav>
+            <ul>
+                <li><Link to="/portfolio/">Home</Link></li>
+                <li><ScrollLink to="about-section" smooth={true} duration={500}>About</ScrollLink></li>
+                <li><ScrollLink to="projects-section" smooth={true} duration={500}>Projects</ScrollLink></li>
+            </ul>
+        </nav>
+    </header>
   );
 }
 
