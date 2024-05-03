b1ac8b473b9e9c6b96a53437662084da23dc4968
Kevan Ngan <kevanngan_@hotmail.com>
2024-05-02 23:51:21 -0700

    Import Footer content

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link as ScrollLink } from 'react-scroll';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
 function App() {
@@ -7,19 +7,11 @@ function App() {
   const restBase = 'https://kevanngan.com/portfolio/wp-json/wp/v2/';
 
   return (
-    <div>
-      <header className="site-header">
-        <nav className="site-navigation">
-          <ul>
-            <li><ScrollLink to="projects-section" smooth={true} duration={500}>Projects</ScrollLink></li>
-            <li><ScrollLink to="about-section" smooth={true} duration={500}>About</ScrollLink></li>
-          </ul>
-        </nav>
-      </header>
-      <main className="main">
-        <Home restBase={restBase} />
-      </main>
-    </div>
+    <Router>
+     <Routes>
+        <Route path="/portfolio/" element={<Home restBase={restBase} />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,27 @@
 const Footer = () => {
+    
+  return (
 
+    <footer className="site-footer">
+        <p>&copy; {new Date().getFullYear()} Kevan Ngan</p>
+
+        <div className="social-icons email">
+            <a href="mailto:kevandngan@gmail.com" target="_blank" rel="noopener noreferrer">
+                <img src="assets/icons/email-icon.png" alt="Email icon" />
+            </a>
+        </div>
+        <div className="social-icons github">
+            <a href="https://github.com/kevanngan" target="_blank" rel="noopener noreferrer">
+                <img src="assets/icons/github-icon.svg" alt="GitHub icon" />
+            </a>
+        </div>
+        <div className="social-icons linkedin">
+            <a href="https://www.linkedin.com/in/kevanngan/" target="_blank" rel="noopener noreferrer">
+                <img src="assets/icons/linkedin-icon.svg" alt="LinkedIn icon" />
+            </a>
+        </div>
+    </footer>
+  );
 }
 
 export default Footer
