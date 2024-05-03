321cb09ae8a8fcdc9bb27d71d2e9a467f1507ee4
Kevan Ngan <kevanngan_@hotmail.com>
2024-05-03 04:43:08 -0700

    Add dropdown faq

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { techStackImages, TechStack } from '../components/TechStack';
+import FAQDropdown from '../components/FaqDropdown';
 
 const About = ({ restBase }) => {
   const postId = 16;
@@ -24,6 +25,18 @@ const About = ({ restBase }) => {
         <article id={`about-section`}>
           <h1>About Me</h1>
           <div className="about-content" dangerouslySetInnerHTML={{ __html: aboutData.content.rendered }}></div>
+          <FAQDropdown 
+            question="What's your favourite genres of music?"
+            answer="I enjoy Future Bass and Melodic dubstep, J-Pop and C-Pop."
+          />
+          <FAQDropdown 
+            question="What are your favourite games?"
+            answer="League of Legends, Teamfight Tactics, and Valorant."
+          />
+          <FAQDropdown 
+            question="What hobby do you want to get into?"
+            answer="I'm looking to get into gardening, I find the thought of just working on a small vegetable garden relaxing and the satisfying when the vegetable or fruits are ripe. (Assuming everything goes well!)"
+          />
           <TechStack images={ techStackImages } />
         </article>
       )}
