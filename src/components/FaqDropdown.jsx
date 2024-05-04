67dfbd9b31dd72ebd801c101877bf26746f429fc
Kevan Ngan <kevanngan_@hotmail.com>
2024-05-03 20:14:55 -0700

    Add rotation on arrows

--- src/components/FaqDropdown.jsx
+++ src/components/FaqDropdown.jsx
@@ -9,7 +9,7 @@ const FAQDropdown = ({ question, answer }) => {
         <div className="faq-dropdown">
             <h3 onClick={toggleOpen}>
                 {question}
-                <span className={`arrow ${isOpen ? 'open' : ''}`}>
+                <span className={`arrow ${isOpen ? 'open' : 'closed'}`}>
                     <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                         <path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z" />
                     </svg>
