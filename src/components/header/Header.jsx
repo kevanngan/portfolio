644d179117eb52cb8ca51620445dcc013c35746d
Kevan Ngan <kevanngan_@hotmail.com>
2024-05-04 19:27:05 -0700

    Add contact in navigation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,6 +11,7 @@ function Header() {
                 <li><Link to="/portfolio/">Home</Link></li>
                 <li><ScrollLink to="about-section" smooth={true} duration={500}>About</ScrollLink></li>
                 <li><ScrollLink to="projects-section" smooth={true} duration={500}>Projects</ScrollLink></li>
+                <li><ScrollLink to="site-footer" smooth={true} duration={500}>Contact</ScrollLink></li>
             </ul>
         </nav>
     </header>
