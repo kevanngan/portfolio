2d889b501462a5857135b5acdf381641d96e85b5
Kevan Ngan <kevanngan_@hotmail.com>
2024-05-06 01:06:28 -0700

    Style footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,22 +3,27 @@ const Footer = () => {
   return (
 
     <footer className="site-footer">
-        <p>&copy; {new Date().getFullYear()} Kevan Ngan</p>
+        <div className="footer-content">
+            <p>&copy; {new Date().getFullYear()} Kevan Ngan</p>
 
-        <div className="social-icons email">
-            <a href="mailto:kevandngan@gmail.com" target="_blank" rel="noopener noreferrer">
-                <img src="assets/icons/email-icon.png" alt="Email icon" />
-            </a>
-        </div>
-        <div className="social-icons github">
-            <a href="https://github.com/kevanngan" target="_blank" rel="noopener noreferrer">
-                <img src="assets/icons/github-icon.svg" alt="GitHub icon" />
-            </a>
-        </div>
-        <div className="social-icons linkedin">
-            <a href="https://www.linkedin.com/in/kevanngan/" target="_blank" rel="noopener noreferrer">
-                <img src="assets/icons/linkedin-icon.svg" alt="LinkedIn icon" />
-            </a>
+            <div className="socials">
+                
+                <div className="social-icons email">
+                    <a href="mailto:kevandngan@gmail.com" target="_blank" rel="noopener noreferrer">
+                        <img src="assets/icons/email-icon.png" alt="Email icon" />
+                    </a>
+                </div>
+                <div className="social-icons github">
+                    <a href="https://github.com/kevanngan" target="_blank" rel="noopener noreferrer">
+                        <img src="assets/icons/github-icon.svg" alt="GitHub icon" />
+                    </a>
+                </div>
+                <div className="social-icons linkedin">
+                    <a href="https://www.linkedin.com/in/kevanngan/" target="_blank" rel="noopener noreferrer">
+                        <img src="assets/icons/linkedin-icon.svg" alt="LinkedIn icon" />
+                    </a>
+                </div>
+            </div>
         </div>
     </footer>
   );
