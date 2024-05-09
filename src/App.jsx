174017ff5324e09898bd0e21784c4e2ad384367c
Kevan Ngan <kevanngan_@hotmail.com>
2024-05-09 16:01:07 -0700

    Import particles.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Home from './pages/Home';
 import BigBoyBurgers from './pages/BigBoyBurgers';
 import Showboat from './pages/Showboat';
 import './scss/styles.scss';
+import ParticlesComponent from './components/ParticlesBackground';
 
 function App() {
   const restBase = 'https://kevanngan.com/portfolio/wp-json/wp/v2/';
 
   return (
+    <>
     <Router>
       <Routes>
         <Route path="/" element={<Home restBase={restBase} />} />
@@ -16,6 +18,8 @@ function App() {
         <Route path="/showboat" element={<Showboat restBase={restBase} />} />
       </Routes>
     </Router>
+    <ParticlesComponent id='particles'/>
+    </>
   );
 }
 
